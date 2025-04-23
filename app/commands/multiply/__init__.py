7f94f4620c74116e6ae3539da09fc570c4bd9e31
Chelsyshankiri <cs688@njit.edu>
2025-04-22 21:59:52 -0400

    Removed unnencessary function calls

--- app/commands/multiply/__init__.py
+++ app/commands/multiply/__init__.py
@@ -2,11 +2,9 @@ from app.commands import Command
 
 
 class MultiplyCommand(Command):
-    def multiply():
+
+    def execute(self):
         numbers = input("Enter two space-separated numbers: ")
         num1, num2 = map(int, numbers.split())
         result = num1 * num2
-        return result
-
-    def execute(self):
-        print("Multiplication result:", MultiplyCommand.multiply())
+        print("Multiplication result:", result)
--- app/commands/subtract/__init__.py
+++ app/commands/subtract/__init__.py
@@ -2,11 +2,9 @@ from app.commands import Command
 
 
 class SubtractCommand(Command):
-    def subtract():
+
+    def execute(self):
         numbers = input("Enter two space-separated numbers: ")
         num1, num2 = map(int, numbers.split())
         result = num1 - num2
-        return result
-
-    def execute(self):
-        print("Subtraction result:", SubtractCommand.subtract())
+        print("Subtraction result:", result)
